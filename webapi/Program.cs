c380b3ee0dc0784a17dfa42f7f6d24ec3adc63cf
set2905 <72752613+set2905@users.noreply.github.com>
2023-11-18 18:54:31 +0400

    serilog

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -1,20 +1,29 @@
+using Application;
+using Persistence;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddApplication()
+                .AddPersistence();
+
+builder.Host.UseSerilog((context, config) =>
+config.ReadFrom.Configuration(context.Configuration));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
