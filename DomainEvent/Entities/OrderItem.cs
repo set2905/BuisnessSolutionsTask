e5ac4ea60bf176b202789e3ac6a3a027b436ed3d
set2905 <72752613+set2905@users.noreply.github.com>
2023-11-18 15:38:43 +0400

    OrderItem

--- DomainEvent/Entities/OrderItem.cs
+++ DomainEvent/Entities/OrderItem.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Ardalis.GuardClauses;
 
-namespace DomainEvent.Entities
+namespace DomainEvent.Entities;
+
+public sealed class OrderItem
 {
-    sealed class OrderItem
+    private OrderItem(OrderItemId id, OrderId orderId, string name, decimal quantity, string unit)
     {
+        Id=id;
+        OrderId=orderId;
+        Name=name;
+        Quantity=quantity;
+        Unit=unit;
+    }
+
+    public OrderItemId Id { get; private set; }
+    public OrderId OrderId { get; private set; }
+    public string Name { get; private set; }
+    public decimal Quantity { get; private set; }
+    public string Unit { get; private set; }
+
+    public static OrderItem Create(OrderItemId id, OrderId orderId, string name, decimal quantity, string unit)
+    {
+        Guard.Against.NullOrEmpty(name, nameof(name), "Order item name cannot be empty");
+        Guard.Against.NullOrEmpty(unit, nameof(unit), "Order item unit cannot be empty");
+        var orderItem = new OrderItem(id, orderId, name, quantity, unit);
+        return orderItem;
     }
 }
+public record OrderItemId(int Value);
