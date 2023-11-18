96f1f60a2136af848e0b0b346a2b4f226d1a2e91
set2905 <72752613+set2905@users.noreply.github.com>
2023-11-18 15:09:03 +0400

    Initial commit

---
+++ DomainEvent/Entities/Order.cs
@@ -0,0 +1,30 @@
+﻿using Ardalis.GuardClauses;
+
+namespace DomainEvent.Entities;
+
+public sealed class Order
+{
+    private readonly HashSet<OrderItem> items = new();
+
+    private Order(OrderId id, string number, DateTime date)
+    {
+        Id=id;
+        Number=number;
+        Date=date;
+    }
+
+    public OrderId Id { get; private set; }
+    public string Number { get; private set; }
+    public DateTime Date { get; private set; }
+    public IReadOnlyCollection<OrderItem> Items => items;
+
+    public static Order Create(OrderId id, string number, DateTime date, ICollection<OrderItem> items)
+    {
+        Guard.Against.NullOrEmpty(number, nameof(number), "The order number must not be empty");
+
+        Order order = new Order(id, number, date);
+        return order;
+    }
+}
+
+public record OrderId(int Value);
---
+++ DomainEvent/Entities/Provider.cs
@@ -0,0 +1,8 @@
+﻿namespace DomainEvent.Entities;
+
+public sealed class Provider
+{
+    public ProviderId Id { get; private set; }
+}
+public record ProviderId(int Value);
+
