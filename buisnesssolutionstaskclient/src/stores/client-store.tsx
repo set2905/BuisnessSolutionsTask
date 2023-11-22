4000171a9bb24885d31e77b2d1160fe5913902eb
set2905 <72752613+set2905@users.noreply.github.com>
2023-11-23 03:04:27 +0400

    Update client-store.tsx

--- buisnesssolutionstaskclient/src/stores/client-store.tsx
+++ buisnesssolutionstaskclient/src/stores/client-store.tsx
@@ -12,6 +12,6 @@ class ClientStore {
 }
 
 export function createClientStore() {
-    const endpoint = process.env.REACT_APP_SERVICE_URI ? process.env.REACT_APP_SERVICE_URI : "https://localhost:7201";
-    return new ClientStore(new Client());
+    const endpoint = "https://localhost:7201";
+    return new ClientStore(new Client(endpoint));
 }
