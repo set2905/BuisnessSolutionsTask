7c2b2e61a0772150a621ade29199eb3c16e148c6
set2905 <72752613+set2905@users.noreply.github.com>
2023-11-22 20:53:06 +0400

    Update clients.ts

--- buisnesssolutionstaskclient/src/clients.ts
+++ buisnesssolutionstaskclient/src/clients.ts
@@ -280,6 +280,47 @@ export class Client {
     }
 
     /**
+     * @param body (optional) 
+     * @return Success
+     */
+    remove(body: OrderId | undefined): Promise<Result> {
+        let url_ = this.baseUrl + "/api/orders/remove";
+        url_ = url_.replace(/[?&]$/, "");
+
+        const content_ = JSON.stringify(body);
+
+        let options_: RequestInit = {
+            body: content_,
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "text/plain"
+            }
+        };
+
+        return this.http.fetch(url_, options_).then((_response: Response) => {
+            return this.processRemove(_response);
+        });
+    }
+
+    protected processRemove(response: Response): Promise<Result> {
+        const status = response.status;
+        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v: any, k: any) => _headers[k] = v); };
+        if (status === 200) {
+            return response.text().then((_responseText) => {
+            let result200: any = null;
+            result200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver) as Result;
+            return result200;
+            });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            });
+        }
+        return Promise.resolve<Result>(null as any);
+    }
+
+    /**
      * @param search (optional) 
      * @return Success
      */
